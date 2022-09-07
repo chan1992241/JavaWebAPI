ad26f01519c31fbe9c1c04b64ad804adda90e84c
chan1992241 <chan1992241@gmail.com>
2022-09-08 03:39:49 +0800

    Initial Commit

---
+++ src/main/java/com/example/practical_test_practice/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.practical_test_practice;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
